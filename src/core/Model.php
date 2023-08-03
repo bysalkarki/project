aa66c866d7a53933eda47bb4d2d4773f7aa8539c
bishal <bishal@bookmundi.com>
2023-08-03 08:57:43 +0545

    added validation

--- src/core/Model.php
+++ src/core/Model.php
@@ -35,15 +35,34 @@ abstract class Model
                 if ($ruleName === self::RULE_REQUIRED && !$value) {
                     $this->addError($attribute, self::RULE_REQUIRED);
                 }
+
+                if ($ruleName === self::RULE_EMAIL && filter_var($value, FILTER_VALIDATE_EMAIL)) {
+                    $this->addError($attribute, self::RULE_EMAIL);
+                }
+
+                if ($ruleName === self::RULE_MIN && strlen($value) < $rule['min']) {
+                    $this->addError($attribute, self::RULE_MIN, $rule);
+                }
+
+                if ($ruleName === self::RULE_MAX && strlen($value) > $rule['min']) {
+                    $this->addError($attribute, self::RULE_MAX, $rule);
+                }
+
+                if ($ruleName === self::RULE_MATCH && $value != $rules[$rule['match']]) {
+                    $this->addError($attribute, self::RULE_MATCH, $rule);
+                }
             }
         }
     }
 
     abstract public function rules(): array;
 
-    public function addError(string $attribute, string $rule): void
+    public function addError(string $attribute, string $rule, array $params = []): void
     {
         $messages = $this->errorMessages()[$rule] ?? '';
+        foreach ($params as $key => $value) {
+            $messages = str_replace("{{$key}}", $value, $messages);
+        }
         $this->errors[$attribute][] = $messages;
     }
 
