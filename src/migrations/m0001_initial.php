53300de34d5b8a53a6403746074657e1f12e714a
bishal <bishal@bookmundi.com>
2023-08-09 19:43:16 +0545

    database migrations

---
+++ src/migrations/m0001_initial.php
@@ -0,0 +1,14 @@
+<?php
+
+class m0001_initial
+{
+    public function up(): void
+    {
+        echo "applying migration";
+    }
+
+    public function down(): void
+    {
+        echo "Down Migration";
+    }
+}
