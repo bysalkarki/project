f43c7a4fc59adb51cacb18863b952b7c28ca9875
bishal <bishal@bookmundi.com>
2023-08-09 21:48:10 +0545

    database done

--- src/migration.php
+++ src/migration.php
@@ -1,2 +1,24 @@
 <?php
 
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use app\core\Application;
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$config = [
+    'db' => [
+        'user'     => $_ENV['DB_USER'],
+        'password' => $_ENV['DB_PASSWORD'],
+        'host'     => $_ENV['DB_HOST'],
+        'port'     => $_ENV['DB_PORT'],
+        'database' => $_ENV['DB_DATABASE']
+    ]
+];
+$app    = new Application(
+    __DIR__, $config
+);
+
+$app->db->applyMigrations();
---
+++ src/migrations/m0001_users.php
@@ -0,0 +1,36 @@
+<?php
+
+
+use app\core\Application;
+use app\core\Database;
+
+class m0001_users
+{
+    public Database $database;
+
+    public function __construct()
+    {
+        $this->database = Application::$app->db;
+    }
+
+    public function up(): void
+    {
+        $sql = "
+       CREATE TABLE users(
+           id INT AUTO_INCREMENT PRIMARY KEY,
+           email varchar(255) NOT NULL,
+           name varchar(255) NOT NULL,
+            status TINYINT NOT NULL DEFAULT 1,
+            password varchar(255) NOT NULL,
+           created_at TIMESTAMP DEFAULT  CURRENT_TIMESTAMP
+       ) ENGINE=INNODB;
+       ";
+        $this->database->pdo->exec($sql);
+    }
+
+    public function down(): void
+    {
+        $sql = "DROP TABLE USERS";
+        $this->database->pdo->exec($sql);
+    }
+}
