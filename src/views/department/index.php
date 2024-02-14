286ed7c76031b44e51b073bf2a6ea1119540df4a
bishal <bishal@bookmundi.com>
2024-02-14 23:14:50 +0545

    modal updated

--- src/views/department/index.php
+++ src/views/department/index.php
@@ -39,10 +39,9 @@
                       <td>
                           <div class="d-flex align-item-center">
                               <a href="/department-update?id=<?= $department->id ?>" class="btn btn-primary btn-sm h-100">Edit</a>
-                              <form action="/department/delete" method="post" class="h-100">
-                                  <input type="hidden" name="id" value="<?= $department->id ?>">
-                                  <button class="btn btn-danger btn-sm" type="submit">Delete</button>
-                              </form>
+                              <button type="button" class="btn btn-sm btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-id="<?= $department->id ?>">
+                                  Delete
+                              </button>
                           </div>
                       </td>
                   </tr>
@@ -55,3 +54,40 @@
 
   </div>
 </div>
+
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <form action="/department/delete" method="post">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Delete Record</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Are you sure you want to delete this record??
+                    <input type="hidden" name="id" value="" id="id">
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<script>
+    const exampleModal = document.getElementById('exampleModal')
+    if (exampleModal) {
+        exampleModal.addEventListener('show.bs.modal', event => {
+// Button that triggered the modal
+            const button = event.relatedTarget
+// Extract info from data-bs-* attributes
+            const id = button.getAttribute('data-bs-id')
+// If necessary, you could initiate an Ajax request here
+// and then do the updating in a callback.
+
+// Update the modal's content.
+            const modalBodyInput = exampleModal.querySelector('#id')
+            modalBodyInput.value = id
+        })
+    }
+</script>
--- src/views/employee/index.php
+++ src/views/employee/index.php
@@ -32,10 +32,9 @@
                         <div class="d-flex align-item-center">
                             <a type="button" href="/employee-update?id=<?= $employee->id ?>"
                                class="btn btn-primary btn-sm h-100">Edit</a>
-                            <form action="/employee/delete" method="post">
-                                <input type="hidden" name="id" value="<?= $employee->id ?>">
-                                <button class="btn btn-danger btn-sm" type="submit">Delete</button>
-                            </form>
+                            <button type="button" class="btn btn-sm btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-id="<?= $employee->id ?>">
+                                Delete
+                            </button>
                         </div>
 
                     </td>
@@ -47,3 +46,40 @@
         </table>
     </div>
 </div>
+
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <form action="/employee/delete" method="post">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Delete Record</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Are you sure you want to delete this record??
+                    <input type="hidden" name="id" value="" id="id">
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<script>
+    const exampleModal = document.getElementById('exampleModal')
+    if (exampleModal) {
+        exampleModal.addEventListener('show.bs.modal', event => {
+// Button that triggered the modal
+            const button = event.relatedTarget
+// Extract info from data-bs-* attributes
+            const id = button.getAttribute('data-bs-id')
+// If necessary, you could initiate an Ajax request here
+// and then do the updating in a callback.
+
+// Update the modal's content.
+            const modalBodyInput = exampleModal.querySelector('#id')
+            modalBodyInput.value = id
+        })
+    }
+</script>
